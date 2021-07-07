3a5de1cdf2373639fad712395cb715a8c2f2d0aa
max <777max8@gmail.com>
2021-07-08 01:07:47 +0300

    changed routers(controllers) for client-app

--- api/routes/post.js
+++ api/routes/post.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
+const { findOne } = require('../models/User')
 const User = require('../models/User')
 
 // create a post
@@ -70,17 +71,28 @@ router.get('/:id', async (req, res) => {
 })
 
 // get timeline posts
-router.get('/timeline/all', async (req, res) => {
+router.get('/timeline/:userId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId)
+    const currentUser = await User.findById(req.params.userId)
     const userPosts = await Post.find({ userId: currentUser._id })
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
-        return  Post.find({ userId: friendId })
+        return Post.find({ userId: friendId })
       })
-      )
-      res.json(userPosts.concat(...friendPosts))
-    } catch (error) {
+    )
+    res.status(200).json(userPosts.concat(...friendPosts))
+  } catch (error) {
+    res.status(500).json(error.message)
+  }
+})
+
+// get user's all posts
+router.get('/profile/:username', async (req, res) => {
+  try {
+    const { _id: userId } = await User.findOne({ userName: req.params.username })
+    const posts = await Post.find({ userId })
+    res.status(200).json(posts)
+  } catch (error) {
     res.status(500).json(error.message)
   }
 })
--- api/routes/users.js
+++ api/routes/users.js
@@ -46,9 +46,13 @@ router.delete('/:id', async (req, res, next) => {
 })
 
 //get a user
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
+  const userId = req.query.userId
+  const userName = req.query.username
   try {
-    const user = await User.findById(req.params.id)
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ userName })
     const { password, updatedAt, ...other } = user._doc
     res.status(200).json(other)
   } catch (error) {
